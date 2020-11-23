afa812b8692876fd0709c83ed3872d7d1598cf41
Romain Tello <romain.tello@gmail.com>
2020-11-23 15:23:16 +0100

    Add colors

--- src/App.java
+++ src/App.java
@@ -2,6 +2,10 @@ public class App
 {
     // Cette méthode est appelée automatiquement au lancement de l'application
     public static void main(String[] args) throws Exception {
+        // Efface la console
+        System.out.print("\033[H\033[2J");   
+        System.out.flush();
+        
         // Créer une nouvelle partie
         Game currentGame = new Game();
 
--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
 
-public class Game
-{
+public class Game {
     private Scanner scanner;
     private boolean isRunning;
 
     private Room[] rooms;
     private Room currentRoom;
 
-    public Game()
-    {
-        // Crée un objet Scanner qui va permettre de traiter les saisies de l'utilisateur
+    public Game() {
+        // Crée un objet Scanner qui va permettre de traiter les saisies de
+        // l'utilisateur
         scanner = new Scanner(System.in);
 
         // Crée les pièces du jeu
-        rooms = new Room[] {
-            new Room("bedroom", "You are in the bedroom"),
-            new Room("bathroom", "You are in the bathroom."),
-            new Room("kitchen", "You are in the kitchen.")
-        };
+        rooms = new Room[] { new Room("bedroom", "You are in the bedroom"),
+                new Room("bathroom", "You are in the bathroom."), new Room("kitchen", "You are in the kitchen.") };
 
         // Définit la pièce de départ
         currentRoom = rooms[0];
@@ -27,23 +23,9 @@ public class Game
         isRunning = true;
     }
 
-    private Room findRoomByName(String name)
-    {
-        // Cherche parmi les pièces disponibles, celle correspondant au nom demandé
-        for (Room room : rooms) {
-            if (room.getName().equals(name)) {
-                return room;
-            }
-        }
-
-        // Si aucune pièce n'a été trouvée, renvoie null
-        return null;
-    }
-
-    public void update()
-    {
+    public void update() {
         // Décrit la pièce actuelle
-        System.out.println(currentRoom.getDescription());
+        System.out.println(ConsoleColor.CYAN + currentRoom.getDescription() + ConsoleColor.RESET);
 
         // Invite l'utilisateur à rentrer une ligne de texte
         System.out.println("");
@@ -56,10 +38,11 @@ public class Game
             return;
         }
 
-        // Cherche si la saisie de l'utilisateur correspond à une pièce, et change de pièce le cas échéant
+        // Cherche si la saisie de l'utilisateur correspond à une pièce, et change de
+        // pièce le cas échéant
         Room newRoom = findRoomByName(userInput);
         if (newRoom == null) {
-            System.out.println("This room does not exist!");
+            System.out.println(ConsoleColor.YELLOW + "This room does not exist!" + ConsoleColor.RESET);
         } else {
             currentRoom = newRoom;
         }
@@ -67,14 +50,24 @@ public class Game
         System.out.println("");
     }
 
-    private void terminate()
-    {
+    private void terminate() {
         isRunning = false;
         scanner.close();
     }
 
-    public boolean getIsRunning()
-    {
+    private Room findRoomByName(String name) {
+        // Cherche parmi les pièces disponibles, celle correspondant au nom demandé
+        for (Room room : rooms) {
+            if (room.getName().equals(name)) {
+                return room;
+            }
+        }
+
+        // Si aucune pièce n'a été trouvée, renvoie null
+        return null;
+    }
+
+    public boolean getIsRunning() {
         return isRunning;
     }
 }
